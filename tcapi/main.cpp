60b359caa57580420fcefcca591afee203d1c230
levanliu <839119912@qq.com>
2025-06-21 08:35:57 +0800

    bazel

--- tcapi/main.cpp
+++ tcapi/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../libB/libB.hpp" // 主程序依赖 libB 的接口
+#include "libB/libB.hpp" // 主程序依赖 libB 的接口
 
 int main() {
     std::cout << "Calling greetFromB from tcapi..." << std::endl;
