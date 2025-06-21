e71767676cafe56815eb2c89e2be2aea2c794b53
levanliu <839119912@qq.com>
2025-06-21 08:23:42 +0800

    d

--- libB/libB.cpp
+++ libB/libB.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "libB.hpp"         // 包含自己的头文件
-#include "../common_include/libA_interface.hpp" // 主程序依赖 libB 的接口
+#include "../libA/libA.hpp" // 主程序依赖 libB 的接口
 
 void greetFromB() {
     greetFromA(); // 调用 libA 的函数
